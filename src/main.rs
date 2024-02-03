ecf43086868338c7493c329b072fb38c4ea49885
jett59 <jett.thompson.59@gmail.com>
2024-02-03 14:40:08 +1100

    add an awareness of the position of the text to be able to put some of the text segmenets on the same line if they have the same y position

--- src/main.rs
+++ src/main.rs
@@ -55,21 +55,53 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut current_font_id = None;
 
     for (page_number, page_id) in document.get_pages() {
+        let mut in_text = false;
+
+        let mut x = 0;
+        let mut y = 0;
+
         for operation in document
             .get_and_decode_page_content(page_id)
             .unwrap()
             .operations
         {
             match operation.operator.as_str() {
+                "BT" => in_text = true,
+                "ET" => in_text = false,
                 "Tf" => {
                     let font_id = operation.operands[0].as_name().unwrap();
                     current_font_id = Some(font_id.to_owned());
                 }
-                "Tj" => {
+                "Tj" if in_text => {
                     let text = operation.operands[0].as_str().unwrap();
                     let font = fonts.get(current_font_id.as_ref().unwrap()).unwrap();
-                    println!("{}", font.decode(text));
+                    print!("{}", font.decode(text));
                 }
+                "Tm" => {
+                    // The matrix is 3x2, where the first two rows give us scaling and stuff, and the third one gives us the position.
+                    let new_x = match operation.operands[4] {
+                        Object::Integer(x) => x as i32,
+                        Object::Real(x) => x as i32,
+                        _ => panic!(
+                            "Expected integer or real, found {:?}",
+                            operation.operands[4]
+                        ),
+                    };
+                    let new_y = match operation.operands[5] {
+                        Object::Integer(y) => y as i32,
+                        Object::Real(y) => y as i32,
+                        _ => panic!(
+                            "Expected integer or real, found {:?}",
+                            operation.operands[5]
+                        ),
+                    };
+                    if new_y > y {
+                        println!();
+                    }
+                    x = new_x;
+                    y = new_y;
+                }
+                //_ if in_text => println!("{:?}", operation.operator.as_str()),
                 _ => {}
             }
         }
